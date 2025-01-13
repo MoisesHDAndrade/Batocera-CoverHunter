d62f4cd5b59e3d2b0ccafce49acb9eb1720be0f8
MoisesHDAndrade <lawliex@gmail.com>
2025-01-13 18:37:44 +1300

    added docstrings

--- the_games_db_scraper.py
+++ the_games_db_scraper.py
@@ -2,17 +2,17 @@ import requests
 from bs4 import BeautifulSoup as bs
 from fake_useragent import UserAgent
 
-# Inicializa o UserAgent para gerar cabeçalhos HTTP dinâmicos
+# Initializes UserAgent to generate dynamic HTTP headers
 ua = UserAgent()
 
 class TheGamesDBScraper():
     """
-    Classe para interagir com o site TheGamesDB, realizando buscas e manipulando os resultados.
+    Class to interact with TheGamesDB website, perform searches, and manipulate results.
     """
 
     def __init__(self, gamename, platform):
         """
-        Inicializa a classe com o atributo 'url', que é uma string vazia.
+        Initializes the class with the game name and platform.
         """
         self.game_name = gamename
         self.platform = platform
@@ -23,107 +23,103 @@ class TheGamesDBScraper():
 
     def base(self, url):
         """
-        Faz uma requisição GET ao site TheGamesDB para buscar informações sobre um jogo específico.
+        Makes a GET request to TheGamesDB website to fetch information about a specific game.
 
         Args:
-            name (str): Nome do jogo a ser pesquisado.
-            platform (str): ID da plataforma no TheGamesDB.
+            url (str): The URL for the request.
 
         Returns:
-            str: Conteúdo HTML da resposta da requisição.
+            str: HTML content of the response.
         """
-        # Constrói a URL com os parâmetros fornecidos
-        url = url
         headers = {
-            "User-Agent": ua.random  # Gera um User-Agent aleatório
+            "User-Agent": ua.random  # Generates a random User-Agent
         }
-        # Realiza a requisição GET
+        # Sends the GET request
         req = requests.get(url, headers=headers)
         return req.text
 
     def soup(self, content):
+        """
+        Parses the HTML content using BeautifulSoup.
+        """
         return bs(content, 'html.parser')
 
     def search_from_thegamesdb(self):
         """
-        Busca informações específicas no site TheGamesDB para um jogo e uma plataforma predefinidos.
+        Searches for specific information on TheGamesDB for a predefined game and platform.
 
         Returns:
-            str: Conteúdo HTML retornado pelo método 'base'.
+            str: HTML content returned by the 'base' method.
         """
-        # Define o ID da plataforma e o nome do jogo
-        platform_id = {
-            "psx": "10"  # ID da plataforma PlayStation 1 (PSX)
-        }
-        name = "Code Veronica"
-        # Busca os dados utilizando o método 'base'
         url = f"https://thegamesdb.net/search.php?name={self.game_name}&platform_id%5B%5D={self.platform}"
-        
         return self.base(url)
 
     def get_results_from_thegamesdb(self):
         """
-        Extrai e exibe a seção do HTML com o ID 'display' retornada pelo site TheGamesDB.
+        Extracts and returns the section of HTML with the ID 'display' returned by TheGamesDB.
 
         Returns:
-            str: Conteúdo HTML da div 'id' retornado pelo método 'search_from_thegamesdb'.
+            str: HTML content of the 'display' div returned by the 'search_from_thegamesdb' method.
         """
-        # Obtém o HTML da busca
+        # Gets the HTML from the search
         info = self.search_from_thegamesdb()
-        # Faz o parsing do HTML usando BeautifulSoup
+        # Parses the HTML using BeautifulSoup
         body = bs(info, 'html.parser')
-        # Busca o elemento com ID 'display'
+        # Finds the element with the ID 'display'
         display = body.find(id="display")
-        # Exibe o conteúdo encontrado
+        # Returns the found content
         return display
 
     def get_games_result_from_thegamesdb(self):
         """
-        Esse metodo ira obter a url para salvar as imagens e descricoes necessarias
+        This method will retrieve the URL for saving images and descriptions as needed.
 
-        Utilizar este metodo para retornar a algum frontend todos os titulos disponiveis
-        Por enquanto ira retornar apenas o primeiro item da lista
+        This method will return the first item in the list for now.
 
         Returns:
-            str: URL da pagina do game
-
-        Prints:
-
-
+            str: URL of the game's page
         """
         display = self.get_results_from_thegamesdb()
         games = display.find_all(class_="col-6")
         if games:
             return games[0].find("a").get("href")
         else:
-            print("Nao achou nenhum resultado para a sua busca")
+            print("No results found for your search")
 
     def get_game_page_info_from_thegamesdb(self):
+        """
+        Retrieves detailed information about the game, such as images and descriptions.
+        """
         game_id = self.get_games_result_from_thegamesdb()
-        url = f"https://thegamesdb.net/{str(game_id).replace("./","")}"
+        url = f"https://thegamesdb.net/{str(game_id).replace('./','')}"
         base = self.base(url)
         body = self.soup(base)
         self.body = body
         images = []
 
         try:
+            # Retrieves game images
             images.append(self.get_clearlogo_from_thegamesdb())
             images.append(self.get_screenshot_from_thegamesdb())
             images.append(self.get_cover_from_thegamesdb())
             return images
         except:
             pass
-       
 
     def get_cover_from_thegamesdb(self):
+        """
+        Retrieves the cover image of the game.
+        """
         body = self.body
         cover_container = body.find_all(attrs={"data-fancybox": "cover"})
         cover_image_url = cover_container[0].find("img").get("src")
         self.covers = cover_image_url
         return cover_image_url
-    
 
     def get_screenshot_from_thegamesdb(self):
+        """
+        Retrieves the screenshot of the game.
+        """
         body = self.body
         screenshot_container = body.find_all(attrs={"data-fancybox": "screenshots"})
         screenshot_image = screenshot_container[0]
@@ -132,22 +128,19 @@ class TheGamesDBScraper():
         return screenshot_image_url
 
     def get_clearlogo_from_thegamesdb(self):
+        """
+        Retrieves the logo image of the game.
+        """
         body = self.body
         clearlogo_container = body.find_all(attrs={"data-fancybox": "clearlogos"})
         clearlogo_image_url = clearlogo_container[0].find_all("img")[0].get("src")
         self.logos = clearlogo_image_url
         return clearlogo_image_url
-        
 
-    
     def get_game_description_from_thegamesdb(self):
+        """
+        Retrieves and prints the description of the game.
+        """
         body = self.get_game_page_info_from_thegamesdb()
         description = body.find_all(class_="game-overview")[0]
         print(description.text)
-
-
-
-
-        
-
-        
